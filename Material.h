bb37ebf49edc4571f31d3ee6940333beed65c205
fuckcpp <m18588290231@163.com>
2019-12-19 16:41:42 +0800

    计算递归反射

--- Material.h
+++ Material.h
@@ -3,16 +3,16 @@
 struct Material
 {
 	Vec3f color;
-	Vec2f albedo;
+	Vec3f albedo;
 	float specular;
 
-	Material(const Vec3f& c,const Vec2f& a,const float& s):albedo(a),specular(s)
+	Material(const Vec3f& c,const Vec3f& a,const float& s):albedo(a),specular(s)
 	{
 		color=c*255;
 	}
 
 	Material(const Vec3f& c)
 	{
-		new(this) Material(c,Vec2f(0.,0.),0.);
+		new(this) Material(c,Vec3f(0.,0.,0.),0.);
 	}
 };
--- raytracer.cpp
+++ raytracer.cpp
@@ -13,7 +13,7 @@
 #include "stb_image.h"
 using namespace std;
 
-const Material bg(Vec3f(0.1,0.5,0.1));
+const Material bg(Vec3f(0.2,0.7,0.8));
 const Material white(Vec3f(1.,1.,1.));
 
 Vec3f reflect(const Vec3f& in,const Vec3f& n)
@@ -41,11 +41,12 @@ bool shadow(const vector<Sphere>& spheres,const Vec3f& hit,const Vec3f& l,const 
 	return isOccluded;
 }
 
-void intersect(const Vec3f& orig,const Vec3f& dir,const vector<Sphere>& spheres,float& distance,Vec3f& hit,Vec3f& norm,Material& mat)
+bool intersect(const Vec3f& orig,const Vec3f& dir,const vector<Sphere>& spheres,Vec3f& hit,Vec3f& norm,Material& mat)
 {
-	float dis=numeric_limits<float>::max();
+	float distance=numeric_limits<float>::max();
 	for(auto& sphere:spheres)
 	{
+		float dis;
 		if(sphere.isRayIntersect(orig,dir,dis)&&dis<distance)
 		{
 			distance=dis;
@@ -54,21 +55,23 @@ void intersect(const Vec3f& orig,const Vec3f& dir,const vector<Sphere>& spheres,
 			mat=sphere.mat;
 		}
 	}
+	return distance<numeric_limits<float>::max();
 }
 
 Vec3f trace(const Vec3f& orig,const Vec3f& dir,const vector<Sphere>& spheres,
-	const vector<PointLight>& pointLights)
+	const vector<PointLight>& pointLights,int depth)
 {
 	Vec3f hit;
 	Vec3f n;
-	float diffIntensity=0;
-	float specIntensity=0;
 	Material mat(Vec3f(1,0,0));
-	float distance=numeric_limits<float>::max();
 	Vec3f color=bg.color;
-	intersect(orig,dir,spheres,distance,hit,n,mat);
-	if(distance<numeric_limits<float>::max())
+	if(depth>0&& intersect(orig,dir,spheres,hit,n,mat))
 	{
+		float diffIntensity=0;
+		float specIntensity=0;
+		Vec3f reflectRay=reflect(dir,n).normalize();
+		Vec3f reflectPoint=reflectRay*n>0?hit+n*1e-3:hit-n*1e-3;
+		Vec3f reflectColor=trace(reflectPoint,reflectRay,spheres,pointLights,depth-1);
 		for(auto& pointLight:pointLights)
 		{
 			Vec3f l=(pointLight.center-hit).normalize();
@@ -79,11 +82,10 @@ Vec3f trace(const Vec3f& orig,const Vec3f& dir,const vector<Sphere>& spheres,
 		}
 	       Vec3f diffColor=mat.color*diffIntensity;
 	       Vec3f specColor=white.color*specIntensity;
-	       color=diffColor*mat.albedo[0]+specColor*mat.albedo[1];
+	       color=diffColor*mat.albedo[0]+specColor*mat.albedo[1]+reflectColor*mat.albedo[2];
 	}
 	tonemapping(color);
 	return color;
-	
 }
 
 void render(const vector<Sphere>& spheres,const vector<PointLight>& pointLights)
@@ -101,6 +103,7 @@ void render(const vector<Sphere>& spheres,const vector<PointLight>& pointLights)
 
 	//rgba 4通道，a取值255
 	//#pragma omp parallel for
+	int recurDepth=4;
 	for(int j=0;j<h;j++)
 	{
 		for(int i=0;i<w;i++)
@@ -111,7 +114,7 @@ void render(const vector<Sphere>& spheres,const vector<PointLight>& pointLights)
 			Dir=Vec3f(x,y,z);
 			Dir.normalize();
 			
-			Vec3f color=trace(Camera,Dir,spheres,pointLights);
+			Vec3f color=trace(Camera,Dir,spheres,pointLights,recurDepth);
 			
 			data[n*(j*w+i)+0]=color.x;
 			data[n*(j*w+i)+1]=color.y;
@@ -128,12 +131,13 @@ int main()
 	vector<Sphere> spheres;
 	vector<PointLight> pointLights;
 
-	Material ivory(Vec3f(0.4, 0.4, 0.3),Vec2f(0.6,0.3),50);
-	Material red_rubber(Vec3f(0.3, 0.1, 0.1),Vec2f(0.9,0.1),10);
+	Material ivory(Vec3f(0.4, 0.4, 0.3),Vec3f(0.6,0.3,0.1),50);
+	Material red_rubber(Vec3f(0.3, 0.1, 0.1),Vec3f(0.9,0.1,0.0),10);
+	Material mirror(Vec3f(1.0,1.0,1.0),Vec3f(0.,10.0,0.8),1425);
 	spheres.emplace_back(Sphere(Vec3f(-3,0,-16),2,ivory));
-	spheres.emplace_back(Sphere(Vec3f(-1,-1.5,-12),2,red_rubber));
-	spheres.emplace_back(Sphere(Vec3f(1.5,-0.5,-18),3,ivory));
-	spheres.emplace_back(Sphere(Vec3f(7,5,-18),4,red_rubber));
+	spheres.emplace_back(Sphere(Vec3f(-1,-1.5,-12),2,mirror));
+	spheres.emplace_back(Sphere(Vec3f(1.5,-0.5,-18),3,red_rubber));
+	spheres.emplace_back(Sphere(Vec3f(7,5,-18),4,mirror));
 	pointLights.emplace_back(PointLight(Vec3f(-10,10,10),2));
 	pointLights.emplace_back(PointLight(Vec3f(30,50,-25),1.8));
 	pointLights.emplace_back(PointLight(Vec3f(30,20,30),1.7));
